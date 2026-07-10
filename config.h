5a8e0d5e5632387c87ec07a5f303dcfb4d774f8c
Ryan Dielhenn <dielhennr@gmail.com>
2026-07-09 20:49:34 -0700

    nits

--- config.h
+++ config.h
@@ -73,14 +73,11 @@ static const char *const autostart[] = {
 static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
-    /* xprop(1):
-     *	WM_CLASS(STRING) = instance, class
-     *	WM_NAME(STRING) = title
-     */
-    /* class      instance    title       tags mask     isfloating   monitor */
-    {"Gimp", NULL, NULL, 0, 1, -1},
+    // This is a noop example; the Firefox binary ships with lowercase title - "firefox"
+    // class      instance    title       tags mask     isfloating   monitor
     {"Firefox", NULL, NULL, 1 << 8, 0, -1},
 };
+
 
 /* layout(s) */
 static const float mfact = 0.55; /* factor of master area size [0.05..0.95] */
